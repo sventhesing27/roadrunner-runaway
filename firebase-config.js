b7fabd55e07b0a4af97a40eb63a8c77f9b246648
sventhesing27 <sventhesing27@gmail.com>
2026-03-09 13:30:06 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,4 @@
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyC888uHhNNp0o_ATP-XYDdyHCTRSdCSkHc",
   authDomain: "roadrunner-runaway.firebaseapp.com",
@@ -14,5 +8,7 @@ const firebaseConfig = {
   appId: "1:479759125781:web:ae2016ddc4bd6fce852d1c"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+const auth = firebase.auth();
